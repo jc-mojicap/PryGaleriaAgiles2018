6ef92220a5f14775f03eafad8f0c47af9782fbb8
Camilo Mojica <jc.mojicap@uniandes.edu.co>
2018-08-19 16:43:28 -0500

    Ajuste para el iframe de audio/video

--- Env/GaleriaVideos/polls/urls.py
+++ Env/GaleriaVideos/polls/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^media/$', views.index, name='index'),
+    url(r'^ver_media/$', views.ver_media, name='verMedia'),
 ]
