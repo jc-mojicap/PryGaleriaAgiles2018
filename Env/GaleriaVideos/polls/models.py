b6325cf86087320654d210b0aeef9c0889ca1dcc
Jorge Martín <je.martin@uniandes.edu.co>
2018-08-24 23:09:20 -0500

    organización de los botones y los links de audio

--- Env/GaleriaVideos/polls/models.py
+++ Env/GaleriaVideos/polls/models.py
@@ -13,14 +13,6 @@ class Categoria(models.Model):
         return self.nombre
 
 
-class Tipo(models.Model):
-    id_tipo = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=30, blank=True)
-
-    def __unicode__(self):
-        return self.nombre
-
-
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30, blank=True)
@@ -44,7 +36,7 @@ class Media(models.Model):
     ciudad = models.CharField(max_length=30, blank=True)
     pais = models.CharField(max_length=30, blank=True)
     categoria = models.ForeignKey(Categoria, null=True)
-    tipo = models.ForeignKey(Tipo, null=True)
+    tipo = models.CharField(max_length=30, blank=True)
     usuario = models.ForeignKey(Usuario, null=True)
 
     def __unicode__(self):
@@ -71,13 +63,6 @@ class Favorito(models.Model):
         return self.id_categoria
 
 
-class TipoSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Tipo
-        fields = ('nombre')
-
-
 class CategoriaSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -95,8 +80,6 @@ class UsuarioSerializer(serializers.ModelSerializer):
 class MediaSerializer(serializers.ModelSerializer):
     owner = UsuarioSerializer(read_only=True)
     ownerId = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Usuario.objects.all(), source='usuario')
-    tipo = TipoSerializer(read_only=True, source='tipo')
-    categoria = TipoSerializer(read_only=True, source='categoria')
 
     class Meta:
         model = Media
