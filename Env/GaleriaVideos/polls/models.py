6ee0881c7f8eac29e70d1d3efa3766dff95e2c19
Jorge Martín <je.martin@uniandes.edu.co>
2018-08-26 01:04:25 -0500

    titulo en el index

--- Env/GaleriaVideos/polls/models.py
+++ Env/GaleriaVideos/polls/models.py
@@ -83,5 +83,5 @@ class MediaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Media
-        fields = ('url', 'titulo', 'autor', 'fecha_creacion', 'ciudad', 'pais', 'categoria', 'tipo', 'usuario')
+        fields = ('url', 'titulo', 'autor', 'fecha_creacion', 'ciudad', 'pais', 'tipo', 'usuario')
 
